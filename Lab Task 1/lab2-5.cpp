6907b321ea39c672113c30638d9d65264822faa2
minhajul21 <147337273+minhajul21@users.noreply.github.com>
2025-11-12 18:46:04 +0600

    Update lab2-5.cpp
    Added input validation for size and improved output formatting.

--- Lab Task 1/lab2-5.cpp
+++ Lab Task 1/lab2-5.cpp
@@ -3,21 +3,34 @@ using namespace std;
 
 int main() {
     int size;
+
     cout << "Enter number of elements: ";
     cin >> size;
+
+    if (size <= 0) {
+        cout << "Invalid number of elements!" << endl;
+        return 0;
+    }
 
     int numbers[size];
     int sum = 0;
 
     cout << "Enter " << size << " numbers:" << endl;
     for (int i = 0; i < size; i++) {
+        cout << "Number " << i + 1 << ": ";
         cin >> numbers[i];
-        sum = sum + numbers[i];
+        sum += numbers[i];
     }
 
-    float average = (float)sum / size;
-    cout << "Average value: " << average << endl;
+    float average = static_cast<float>(sum) / size;
+
+    cout << "\n------------------\n";
+    cout << "Total Sum   = " << sum << endl;
+    cout << "Total Count = " << size << endl;
+    cout << "Average     = " << average << endl;
+    cout << "-------------------\n";
 
     return 0;
 }
+
 
